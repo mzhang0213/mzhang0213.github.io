7e04e8580f1c7395a9d323cc154705c26b4f8a6b
mzhang0213 <85912706+mzhang0213@users.noreply.github.com>
2023-11-13 11:18:24 -0500

    tests schematic and test 1

---
+++ schematic.js
@@ -0,0 +1,13 @@
+//tests
+var tests = {
+	test_number:{
+		questions:["start","end"],
+		exceptions:["question_number"],
+		prompts:[
+			{
+				question:["numbers"],
+				parts:["empty or a + b + c + ..."]
+			}
+		]
+	}
+}
